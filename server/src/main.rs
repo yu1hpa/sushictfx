34f98c2440efa77958fc57922977bd6ac0ef950f
hiro-1219 <hiro236_prog-pc@outlook.jp>
2023-07-04 17:11:55 +0900

    fix: fix Dockerfile, server run

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,31 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod models;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use mongodb::Client;
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+/*#[get("/api/v1/teams")]
+async fn getTeams(client: web::Data<Client>) -> impl Responder {
+
+}*/
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Server Start");
     let client = Client::with_uri_str("mongodb://0.0.0.0:27017")
         .await
         .expect("Failed to connect client");
-    let _db = client.database("sushictfx-db");
-    HttpServer::new(move || App::new())
-        .bind("0.0.0.0::8888")?
-        .run()
-        .await
+    //let _db = client.database("sushictfx-db");
+    HttpServer::new(move || 
+        App::new()
+            .app_data(web::Data::new(client.clone()))
+            .service(hello)
+    )
+    .bind(("0.0.0.0", 8888))?
+    .run()
+    .await
 }
