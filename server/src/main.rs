7d97a52872b9a20ea1352ce1901e15abb6d56c11
hiro-1219 <hiro236_prog-pc@outlook.jp>
2023-07-05 14:53:25 +0900

    fix: scoreserverのDockerfileとdocker-compose.yamlを変更

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,11 @@
 mod models;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    get, 
+    web, App, HttpResponse, HttpServer, Responder
+};
 use mongodb::Client;
+use models::teams::Team;
+
 
 
 #[get("/")]
@@ -9,17 +14,56 @@ async fn hello() -> impl Responder {
 }
 
 /*#[get("/api/v1/teams")]
-async fn getTeams(client: web::Data<Client>) -> impl Responder {
+async fn getTeams(client: web::Data<Client>){
 
 }*/
+
+/*#[post("/api/v1/teams")]
+async fun postTeams(client: web::Data<Client>) { 
+
+}*/
+
+async fn migrate_database(client: &Client) {
+    let db = client.database("sushictfx-db");
+    let collection = db.collection::<Team>("teams");
+    let teams = vec![
+        Team{
+            id: None,
+            role: "NotAdmin".to_string(),
+            name: "Yajirushi314".to_string(),
+            password: "pass12345".to_string(),
+            email: "yajirushi@test.com".to_string(),
+            points: 100,
+            solved_ids: vec![
+                1, 2, 3, 4, 5
+            ]
+        },
+        Team{
+            id: None,
+            role: "Admin".to_string(),
+            name: "sush1st".to_string(),
+            password: "pass67890".to_string(),
+            email: "sush1st@test.com".to_string(),
+            points: 300,
+            solved_ids: vec![
+                6, 7, 8, 9, 10
+            ]
+        },
+    ];
+    collection
+        .insert_many(teams, None)
+        .await
+        .expect("insert migrate document");
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server Start");
-    let client = Client::with_uri_str("mongodb://0.0.0.0:27017")
+    let client = Client::with_uri_str("mongodb+srv://root:example@0.0.0.0:27017")
         .await
         .expect("Failed to connect client");
     //let _db = client.database("sushictfx-db");
+    migrate_database(&client).await;
     HttpServer::new(move || 
         App::new()
             .app_data(web::Data::new(client.clone()))
--- server/src/models/teams.rs
+++ server/src/models/teams.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
+use mongodb::bson::oid::ObjectId;
 
 #[derive(Serialize, Deserialize)]
 pub struct Team {
-    id: String,
-    role: String,
-    name: String,
-    password: String,
-    email: String,
-    points: uint32,
-    solvedIds: Vec<uint32>,
+    #[serde(rename = "_id", skip_serializing)]
+    pub id: Option<ObjectId>,
+    pub role: String,
+    pub name: String,
+    pub password: String,
+    pub email: String,
+    pub points: u32,
+    pub solved_ids: Vec<u32>,
 }
